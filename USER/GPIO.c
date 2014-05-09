f54cb3aec3d5e058f787d86000d2ee94bdc27b50
jason <the.jason.cn@gmail.com>
2014-05-09 13:42:13 +0800

    little change for only some spaces

--- USER/GPIO.c
+++ USER/GPIO.c
@@ -97,7 +97,7 @@ void GPIO_Config(void)
 	
 	//I2C GPIO
 	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 | GPIO_Pin_7;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
