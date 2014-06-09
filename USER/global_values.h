89ed7353d74951018c8525c622b8e00cbfd79fc8
jason <the.jason.cn@gmail.com>
2014-06-09 11:17:12 +0800

    self check function was added and 0101 for forward first and 1010 for backward first--synced from Hasee by KuaiPan

--- USER/global_values.h
+++ USER/global_values.h
@@ -12,33 +12,66 @@
 	unsigned char 	IDOfPCB = 0;
 	unsigned char 	RunMode = 0;
 	unsigned char 	I2CStatus = 0;
-	unsigned int 	PtoEtcSWCheckResult = 0;
+	unsigned int 		PtoEtcSWCheckResult = 0;
 	
 	PROTOCOL_t gU2RecvBuff;//usart information
 	
-
-	#define UploadPCBID 		0x01 		//upload PCB ID 
-	#define CheckPtoElcSW		0x02		//check photoelectric switch
-	#define MotorForward 		0x03 		
-	#define MotorBackward 	0x04
-	#define	StopAllMotor		0x05		//stop all motor
-	#define ReverseRowMotor 0x06
-	#define StartPtoEtcSW		0x07		//start photoelectric switch
-	#define StopPtoEtcSW		0x08		//stop photoelectric switch
-	#define CheckPtoEtcSW 	0x09
-	#define StartLED				0x0A
-	#define StopLED					0x0B	
+	#define RunModeNormal 	0x00
+	#define RunModeTest 		0x0A
 	
-	#define ResultOfPtoEtcSW	0x22
+	#define UploadPCBID 		0x01 		//not available 
+	#define MotorForward 		0x03 		//
+	#define MotorBackward 		0x04		//
+	#define	StopAllMotor		0x05		//
+	#define ReverseRowMotor 	0x06        //not available
+	#define StartPtoEtcSW		0x07		//
+	#define StopPtoEtcSW		0x08		//
+	#define CheckPtoEtcSW 		0x09
+	#define StartLED			0x0A        //
+	#define StopLED				0x0B	    //
+	unsigned char HandShakeToMaster[7] = 			{0xAA, 0xBB, 00, 00, 00, 00, 0x65};//
+	unsigned char PCBID[7] = 						{0xAA, 0x01, 00, 00, 00, 00, 0xAB};//
+	unsigned char MedecineFailed[7] = 				{0xAA, 0x02, 00, 00, 00, 00, 0xAC};//
+	unsigned char MedecineSuccedded[7] = 			{0xAA, 0x03, 00, 00, 00, 00, 0xAD};//
+	unsigned char StarLEDSuccedded[7] = 			{0xAA, 0x04, 00, 00, 00, 00, 0xAE};//
+	unsigned char StopLEDSuccedded[7] = 			{0xAA, 0x05, 00, 00, 00, 00, 0xAF};//
+	unsigned char StartPtoEtcSWSuccedded[7]= 		{0xAA, 0x06, 00, 00, 00, 00, 0xB0};//
+	unsigned char StopPtoEtcSWSuccedded[7] = 		{0xAA, 0x07, 00, 00, 00, 00, 0xB1};//
+	unsigned char PtoEtcSWCheckResInfo[7] = 		{0xAA, 0x08, 00, 00, 00, 00, 0xB2};//
+	unsigned char AllMotorStopped[7] = 				{0xAA, 0x09, 00, 00, 00, 00, 0xB3};//
+	unsigned char ColCurrentOverRange[7] = 			{0xAA, 0x0A, 00, 00, 00, 00, 0xB4};//
+	unsigned char MotorStartInfo[7] = 				{0xAA, 0x0B, 00, 00, 00, 00, 0xB5};//
 	
 	unsigned int motor_col = 0;
 	unsigned int motor_row = 0;
 	unsigned int MeanRunningCurrent = 0;
-	
-	unsigned char HandShakeToMaster[7] = {0xAA, 0xBB, 00, 00, 00, 00, 00};
-	//unsigned char StarLEDBack[7] = {0xAA, 0x....};
-	//unsigned char StopLEDBack[7] = {};
-	
+		
 	volatile u32 step_timer2;
 	unsigned char WaitPtoEtcSW = 0;
+	unsigned int DelayTimeOfTimer2 = 1000; 
+	
+	typedef enum
+	{
+		MotorStoppedTop = 1,
+		KeyPushed,
+		MotorStartDown,
+		DownLimSW,
+		MotorStoppedBottom,
+		InfraredSensorStatus,
+		TimerStarted,
+		TimerTerminated,
+		MotorstartUp,
+		UpLimSW,
+	}TchScrSltStatus_TypeDef;
+	TchScrSltStatus_TypeDef TchScrSltStatus = MotorStoppedTop;
+	
+	
+	typedef enum
+	{
+		Stopped = 0,
+		Forward = 1,
+		Backward = 2,
+	}MotorStatus_TypeDef;
+	
+	MotorStatus_TypeDef MotorStatus = Stopped;
 #endif
